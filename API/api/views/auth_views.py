8c3d27396ecea8c1879b905ac4097d69a25947a2
Dominic Spinks <dominic.spinks@outlook.com>
2025-03-29 22:07:17 +1100

    Restructure views

--- API/api/views/auth_views.py
+++ API/api/views/auth_views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import get_user_model
-from .serializers import RegisterSerializer, EmailTokenObtainSerializer
+from ..serializers import RegisterSerializer, EmailTokenObtainSerializer
 from rest_framework.views import APIView
 
 User = get_user_model()

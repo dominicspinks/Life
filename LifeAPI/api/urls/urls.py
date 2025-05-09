ed4b6b434da04f120d4b266f80b8246274befbbd
Dominic Spinks <dominic.spinks@outlook.com>
2025-05-09 10:46:33 +1000

    Refactor urls into separate folder

--- LifeAPI/api/urls/urls.py
+++ LifeAPI/api/urls/urls.py
@@ -6,11 +6,11 @@ router = DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('api.urls_auth')),
-    path('modules/', include('api.urls_modules')),
-    path('lists/', include('api.urls_lists')),
-    path('reference/', include('api.urls_reference')),
-    path('budgets/', include('api.urls_budgets')),
+    path('auth/', include('api.urls.urls_auth')),
+    path('modules/', include('api.urls.urls_modules')),
+    path('lists/', include('api.urls.urls_lists')),
+    path('reference/', include('api.urls.urls_reference')),
+    path('budgets/', include('api.urls.urls_budgets')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger')
 ]

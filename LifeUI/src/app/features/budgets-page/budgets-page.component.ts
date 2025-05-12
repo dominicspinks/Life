c52fbc5c328fb141c1536d40201a58a0cb41f7ab
Dominic Spinks <dominic.spinks@outlook.com>
2025-05-12 12:04:34 +1000

    Add tab menu to budgets page

--- LifeUI/src/app/features/budgets-page/budgets-page.component.ts
+++ LifeUI/src/app/features/budgets-page/budgets-page.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { LoggerService } from '../../core/services/logger.service';
 
 @Component({
     selector: 'app-budgets-page',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive],
     templateUrl: './budgets-page.component.html',
     styleUrl: './budgets-page.component.css'
 })
 export class BudgetsPageComponent {
+    private logger = inject(LoggerService);
+    private router = inject(Router);
+    private route = inject(ActivatedRoute);
 
+    currentTab = 'summary';
+
+    onTabChange(event: Event) {
+        const selectedTab = (event.target as HTMLSelectElement).value;
+        this.currentTab = selectedTab;
+        this.router.navigate([selectedTab], { relativeTo: this.route });
+    }
 }
